7dbc882bef23691b6b319d72abfd179a05603d6f
Alexander Ceron <alexander.ceron16@gmail.com>
2022-09-12 22:23:31 -0300

    Update

--- pages/github.jsx
+++ pages/github.jsx
@@ -52,21 +52,21 @@ const GithubPage = ({ repos, user }) => {
 
 export async function getStaticProps() {
   const userRes = await fetch(
-    `https://api.github.com/users/${gitData.user}`,
+    `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME}`,
     {
       headers: {
-        Authorization: `token ${gitData.token}`,
+        Authorization: `token ${process.env.GITHUB_API_KEY}`,
       },
     }
   );
   
   const user = await userRes.json();
-  console.log(user);
+
   const repoRes = await fetch(
-    `https://api.github.com/users/${gitData.user}/repos?per_page=100`,
+    `https://api.github.com/users/${process.env.NEXT_PUBLIC_GITHUB_USERNAME}/repos?per_page=100`,
     {
       headers: {
-        Authorization: `token ${gitData.token}`,
+        Authorization: `token ${process.env.GITHUB_API_KEY}`,
       },
     }
   );
